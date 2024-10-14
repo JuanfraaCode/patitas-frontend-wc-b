4a712129181beab570bdcf0af96f5f6985c24844
JuanfraaCode <jf7cabrera@gmail.com>
2024-10-13 22:42:44 -0500

    Examen T3 - Cerrado de Sesión con Cloud

--- src/main/java/pe/edu/cibertec/patitas_frontend_wc/controller/LoginControllerAsync.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc/controller/LoginControllerAsync.java
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
 import pe.edu.cibertec.patitas_frontend_wc.dto.LoginRequestDTO;
 import pe.edu.cibertec.patitas_frontend_wc.dto.LoginResponseDTO;
+import pe.edu.cibertec.patitas_frontend_wc.dto.LogoutRequestDTO;
+import pe.edu.cibertec.patitas_frontend_wc.dto.LogoutResponseDTO;
+import pe.edu.cibertec.patitas_frontend_wc.viewmodel.LoginModel;
 import reactor.core.publisher.Mono;
 
 @RestController
@@ -23,7 +26,7 @@ public class LoginControllerAsync {
                 loginRequestDTO.numeroDocumento() == null || loginRequestDTO.numeroDocumento().trim().length() == 0 ||
                 loginRequestDTO.password() == null || loginRequestDTO.password().trim().length() == 0){
 
-            return Mono.just(new LoginResponseDTO("01", "Error: Debe completar correctamente sus credenciales", "",""));
+            return Mono.just(new LoginResponseDTO("01", "Error: Debe completar correctamente sus credenciales", "","","",""));
         }
 
         try {
@@ -36,15 +39,39 @@ public class LoginControllerAsync {
                     .flatMap(response -> {
 
                         if(response.codigo().equals("00")){
-                            return Mono.just(new LoginResponseDTO("00", "", response.nombreUsuario(), ""));
+                            return Mono.just(new LoginResponseDTO("00", "", loginRequestDTO.tipoDocumento(), loginRequestDTO.numeroDocumento(), response.nombreUsuario(), response.correoUsuario()));
                         } else {
-                            return Mono.just(new LoginResponseDTO("02", "Error: Autenticacion fallida", "", ""));
+                            return Mono.just(new LoginResponseDTO("02", "Error: Autenticacion fallida", "","","", ""));
                         }
                     });
         } catch(Exception e) {
             System.out.println(e.getMessage());
-            return Mono.just(new LoginResponseDTO("99", "Error: Ocurrio un problema en la autenticacion", "", ""));
+            return Mono.just(new LoginResponseDTO("99", "Error: Ocurrio un problema en la autenticacion","","", "", ""));
         }
     }
 
+    @PostMapping("/logout-async")
+    public Mono<LogoutResponseDTO> logout(@RequestBody LogoutRequestDTO logoutRequestDTO){
+
+        if (logoutRequestDTO.tipoDocumento() == null || logoutRequestDTO.tipoDocumento().trim().length() == 0 ||
+                logoutRequestDTO.numeroDocumento() == null || logoutRequestDTO.numeroDocumento().trim().length() == 0){
+
+            LoginModel loginModel = new LoginModel("99", "Error: debe completar sus credenciales correctamente","");
+
+            return Mono.just(new LogoutResponseDTO(false, null, "Error: debe completar sus credenciales correctamente"));
+    }
+        try {
+            return webClientAutenticacion.post().uri("/logout").body(Mono.just(logoutRequestDTO), LoginResponseDTO.class)
+                    .retrieve().bodyToMono(LogoutResponseDTO.class)
+                            .flatMap(response ->{
+                if (response.resultado().equals(true)){
+                    return Mono.just(new LogoutResponseDTO(true, response.fecha(), response.msjError()));
+                } else
+                    return Mono.just(new LogoutResponseDTO(false,null, "Error: no se pudo cerrar sesión"));
+            });
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+            return Mono.just(new LogoutResponseDTO(false, null,"Error: en logout"));
+        }
+    }
 }
--- src/main/java/pe/edu/cibertec/patitas_frontend_wc/dto/LoginResponseDTO.java
+++ src/main/java/pe/edu/cibertec/patitas_frontend_wc/dto/LoginResponseDTO.java
@@ -3,5 +3,6 @@ package pe.edu.cibertec.patitas_frontend_wc.dto;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record LoginResponseDTO(String codigo, String mensaje, String nombreUsuario, String correoUsuario) {
+public record LoginResponseDTO(String codigo, String mensaje, String tipoDocumento, String numeroDocumento,
+                               String nombreUsuario, String correoUsuario) {
 }
